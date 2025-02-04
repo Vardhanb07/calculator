9cb0c335e561e63e713504992adde2c8c33c32ea
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-02-04 19:30:04 +0530

    Add code for operations

---
+++ script.js
@@ -0,0 +1,44 @@
+function addNumbers(number1, number2){
+    return number1 + number2;
+}
+function subNumbers(number1, number2){
+    return number1 - number2;
+}
+function multiplyNumbers(number1, number2){
+    return number1 * number2;
+}
+function divideNumbers(number1, number2){
+    if(!(number2 == 0)){
+        return number1 / number2;
+    }
+    return 'Cannot divide by zero';
+}
+function operate(number1, number2, operater){
+    switch(operater) {
+        case '+' :
+            addNumbers(number1, number2);
+            break;
+        case '-' :
+            subNumbers(number1, number2);
+            break;
+        case '/' :
+            divideNumbers(number1, number2);
+            break;
+        case '*' :
+            multiplyNumbers(number1, number2);
+            break;
+        default:
+            console.log('Unknown operation');
+    }
+}
+const buttons = document.querySelector('.buttons');
+for(let i = 0; i < 5; i++){
+    let head = document.createElement('div');
+    head.classList.add('head');
+    for(let j = 0; j < 4; j++){
+        let element = document.createElement('div');
+        element.classList.add('element');
+        head.appendChild(element);
+    }
+    buttons.appendChild(head);
+}
